c78a987788b751e2c84764167745672a92fc60bb
Weerachai <s6102041520165@email.kmutnb.ac.th>
2019-12-31 07:42:44 +0700

    used adminlte theme

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,6 +37,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                   '@app/views' => '@backend/theme'
+                ],
+            ],
+       ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
